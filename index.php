2d41c397e71d6ce864f086cf143d81ac3a3a4add
danielzarazua <danielzarazuaflores@hotmail.com>
2019-04-20 22:58:44 -0500

    UPDATE ME EQUIVOQUE

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 
-"Hola, mi nombre es Daniel. Soy altruista y me encanta la naturaleza. Tambien 
+
+echo "Hola, mi nombre es Daniel. Soy altruista y me encanta la naturaleza. Tambien 
 soy amante de la tecnología. Me gustaría ser un experto en seguridad de la 
 información, creo que es un area con mucho crecimiento y con gran complejidad. Es todo un reto. "
+
 
 
 ?>
