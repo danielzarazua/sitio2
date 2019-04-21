74efead83866ad268fac593b13affd46f648b61f
danielzarazua <danielzarazuaflores@hotmail.com>
2019-04-20 22:47:44 -0500

    ADD:archivo de presentacion

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+"Hola, mi nombre es Daniel. Soy altruista y me encanta la naturaleza. Tambien 
+soy amante de la tecnología. Me gustaría ser un experto en seguridad de la 
+información, creo que es un area con mucho crecimiento y con gran complejidad. Es todo un reto. "
+
+
+?>
